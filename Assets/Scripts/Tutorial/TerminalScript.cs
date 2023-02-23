f69a85c1f3768f191a486ef055e9bc9681f3de0e
Brahim006 <brahimeduali@gmail.com>
2023-02-23 13:09:28 -0300

    Añadida mecánica de instanciación de partes robóticas

--- Assets/Scripts/Tutorial/TerminalScript.cs
+++ Assets/Scripts/Tutorial/TerminalScript.cs
@@ -1,27 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TerminalScript : MonoBehaviour
 {
-    [SerializeField] LevelManager levelManager;
-    private float _timeFromLastInstanciation = 0;
+    [SerializeField] private GameObject memberPartsInstantiator;
+    [SerializeField] private GameObject corePartsInstantiator;
+    [SerializeField] private GameObject[] memberPartsPrefabs;
+    [SerializeField] private GameObject[] corePartsPrefabs;
+    private Queue<GameObject> memberPartsQueue = new Queue<GameObject>();
+    private Queue<GameObject> corePartsQueue = new Queue<GameObject>();
 
-    private void Update()
+    private static readonly int PARTS_PER_QUEUE = 3;
+
+    private void Start()
     {
-        _timeFromLastInstanciation += Time.deltaTime;
+        FillRobotPartsQueues();
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.TryGetComponent<CharacterController>(out CharacterController player))
+        var areQueuesEmpty = memberPartsQueue.Count == 0 || corePartsQueue.Count == 0;
+        if(
+            Input.GetKeyDown(KeyCode.E) &&
+            !areQueuesEmpty &&
+            other.TryGetComponent<CharacterController>(out CharacterController player)
+          )
         {
-            if(Input.GetKeyDown(KeyCode.E) && _timeFromLastInstanciation > 1)
-            {
-                levelManager.InstanciateRobotParts();
-                _timeFromLastInstanciation = 0;
-            }
+            player.PressButton(transform.position);
+            InstantiateRobotParts();
         }
     }
 
+    private void InstantiateRobotParts()
+    {
+        Instantiate(memberPartsQueue.Dequeue(), memberPartsInstantiator.transform.position, memberPartsInstantiator.transform.rotation);
+        Instantiate(corePartsQueue.Dequeue(), corePartsInstantiator.transform.position, corePartsInstantiator.transform.rotation);
+    }
+
+    private void FillRobotPartsQueues()
+    {
+        for(int i = 0; i < PARTS_PER_QUEUE; i++)
+        {
+            memberPartsQueue.Enqueue(memberPartsPrefabs[Random.Range(0, memberPartsPrefabs.Length)]);
+            corePartsQueue.Enqueue(corePartsPrefabs[Random.Range(0, corePartsPrefabs.Length)]);
+        }
+    }
 }
