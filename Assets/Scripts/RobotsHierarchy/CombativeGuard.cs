e8b7d8f4d64e63529c6fd42a81a65983587f3f16
Brahim006 <brahimeduali@gmail.com>
2023-03-27 20:05:07 -0300

    Añadidas animaciones del robot enemigo

--- Assets/Scripts/RobotsHierarchy/CombativeGuard.cs
+++ Assets/Scripts/RobotsHierarchy/CombativeGuard.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class CombativeGuard : CombativeRobot
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
